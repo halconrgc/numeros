dcb894ebc9aca18fdabaf4a4b1fb07d640fe18be
halco <halco@192.168.0.85>
2021-09-25 01:16:34 -0500

    creacion de los cases

--- src/main/java/com/mycompany/modelado/Principal.java
+++ src/main/java/com/mycompany/modelado/Principal.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.modelado;
-
 import java.util.Scanner;
-
-/**
- *
- * @author halco
- */
 public class Principal {
     public static void main(String[] args) {
         Automovil MiAutomovil =new Automovil("dodge", "verna","verde", 5, 4, 40);
@@ -19,11 +8,30 @@ public class Principal {
         int opc=1;
         Scanner scanner=new Scanner(System.in);
         do {
+            System.out.println("Menu operaciones basicas "
+                    + "1.- suma "
+                    + "2.-resta "
+                    + "3.-multiplicacion "
+                    + "4.- division "
+                    + "5.-modulo");
             System.out.println("Dame una opcion");
-            
             opc=scanner.nextInt();
+            
+            switch (var) {
+                case 1:                    
+                    break;
+                case 2:                    
+                    break;
+                case 3:                    
+                    break;
+                case 4:                    
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+    
         }
         while(opc!=8); 
-        
+  
     }
 }
